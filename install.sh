f300e33baa5dcb836604d65522785a4750db1e82
Andy Blight <andy.blight@gmail.com>
2018-02-10 00:42:54 +0000

    Added ubuntu17.10 scripts

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
-# Installs the scripts in the $1 directory.
+# Installs the scripts from the $1 directory.
+
 set -e
 
 if [ -d $1 ]
 then
   echo "Installing scripts from $1 to home directory"
-  cp $1/.bash_aliases ~ 
-  cp -R $1/bin ~
+  cp $1/.gitconfig ${HOME}
+  cp $1/.bash_aliases ${HOME}
+  cp -R $1/bin ${HOME}
 else
   echo "No directory '$1'"
 fi
