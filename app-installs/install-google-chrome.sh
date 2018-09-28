bd98fec270a13857662ad72dc63aba44f837ed54
Andy Blight <andy.blight@gmail.com>
2018-09-28 11:38:25 +0100

    Moved all application installation scripts to app-installs directory

---
+++ app-installs/install-google-chrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# From: https://askubuntu.com/questions/991583/how-to-install-google-chrome-from-terminal
+
+# Add key
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+
+# Add repo
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+# Update and install
+sudo apt update
+sudo apt install google-chrome-stable
--- ubuntu14.04lts/bin/upgrade.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get autoremove -y
-sudo apt-get autoclean -y
