958321b77fbc11bb5e10b0d7bba85d5dc1380b5d
Andy Blight <andy.blight@gmail.com>
2018-09-28 09:40:11 +0100

    Added install script for skypeforlinux

---
+++ ubuntu17.10/bin/install-skype.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# From: https://askubuntu.com/questions/887389/how-to-install-skype-for-linux-in-ubuntu-16-04-via-console-only
+set -e
+
+# Download key GPG key
+curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+
+# Add Skype to the list of Apt repositories
+echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
+
+# Install Skype for Linux
+sudo apt update
+sudo apt install skypeforlinux
