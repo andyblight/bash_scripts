305dfc337ef77feaf8ef0565598160a4c56c1a19
Andy Blight <andy.blight@gmail.com>
2020-04-27 08:51:32 +0100

    Fixed problem with vscode install.
    setting.json was not being copied correctly.
    Removed install-all.sh in favour of all.sh.

--- desktop-installs/install-vscode.sh
+++ desktop-installs/install-vscode.sh
@@ -11,6 +11,7 @@ then
   wget https://packages.microsoft.com/keys/microsoft.asc -O - | gpg --dearmor > microsoft.gpg
   sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
   echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee -a ${APT_FILE}
+  rm microsoft.gpg
 fi
 
 # Update cache with the new repo and install.
@@ -18,4 +19,5 @@ sudo apt update
 sudo apt install -y code
 
 # Copy settings file.
-cp ./settings.json ${USER}/.config/Code/User
+mkdir -p /home/${USER}/.config/Code/
+cp ./settings.json /home/${USER}/.config/Code/User
