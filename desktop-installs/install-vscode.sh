fa8dca787ce71879cb5c08f432d51afcf48d425f
Andy Blight <andy.blight@gmail.com>
2020-05-13 13:35:52 +0100

    Updated vscode install.
    Fixed problem installing basic settings.  Now uses snap.

--- desktop-installs/install-vscode.sh
+++ desktop-installs/install-vscode.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 # From: https://code.visualstudio.com/docs/setup/linux
-
 set -e
 
-# Add apt files
-APT_FILE=/etc/apt/sources.list.d/vscode.list
-if [ ! -e ${APT_FILE} ]
-then
-  echo "Adding apt files"
-  wget https://packages.microsoft.com/keys/microsoft.asc -O - | gpg --dearmor > microsoft.gpg
-  sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-  echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" | sudo tee -a ${APT_FILE}
-  rm microsoft.gpg
-fi
-
-# Update cache with the new repo and install.
-sudo apt update
-sudo apt install -y code
+# Install using snap
+sudo snap install --classic code
 
 # Copy settings file.
-mkdir -p /home/${USER}/.config/Code/
-cp ./settings.json /home/${USER}/.config/Code/User
+mkdir -p /home/${USER}/.config/Code/User
+cp -f ./settings.json /home/${USER}/.config/Code/User/settings.json
