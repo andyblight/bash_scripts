15f027a929b5f25ab20ce1c719bdf752d017bee1
Andy Blight <andy.blight@gmail.com>
2020-06-03 15:09:26 +0100

    Added vscode watched file fix

--- desktop-installs/install-vscode.sh
+++ desktop-installs/install-vscode.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # From: https://code.visualstudio.com/docs/setup/linux
-set -e
+set -ex
 
 # Install using snap
 sudo snap install --classic code
+
+# Increase watched file limit.
+echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
 
 # Copy settings file.
 mkdir -p /home/${USER}/.config/Code/User
